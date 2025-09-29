d0d84f385d58e80bc7f9b4e600470f3f94a49192
Bertold Vincze <54671528+welle-dev-duck@users.noreply.github.com>
2025-09-29 15:18:37 +0200

    feat: initial commit

---
+++ dev/app/(payload)/admin/importMap.js
@@ -0,0 +1,9 @@
+import { BeforeDashboardClient as BeforeDashboardClient_fc6e7dd366b9e2c8ce77d31252122343 } from 'plugin-custom-fields/client'
+import { BeforeDashboardServer as BeforeDashboardServer_c4406fcca100b2553312c5a3d7520a3f } from 'plugin-custom-fields/rsc'
+
+export const importMap = {
+  'plugin-custom-fields/client#BeforeDashboardClient':
+    BeforeDashboardClient_fc6e7dd366b9e2c8ce77d31252122343,
+  'plugin-custom-fields/rsc#BeforeDashboardServer':
+    BeforeDashboardServer_c4406fcca100b2553312c5a3d7520a3f,
+}
